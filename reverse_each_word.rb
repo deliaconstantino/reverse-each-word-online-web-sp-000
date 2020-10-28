1156c5c4df7157356602df066b7dc1c996b8f5ab
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:49:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  org_array = sentence.split(" ")
-  reversed_array = []
-    org_array.each do |org_word|
-      reversed_array << org_word.reverse
-    end
-  reversed_array.join(" ")
-end
+# def reverse_each_word(sentence)
+#   org_array = sentence.split(" ")
+#   reversed_array = []
+#     org_array.each do |org_word|
+#       reversed_array << org_word.reverse
+#     end
+#   reversed_array.join(" ")
+# end
 
 # def reverse_each_word(sentence)
 #   array = sentence.split(" ")
@@ -14,3 +14,7 @@ end
 #     end
 #   reversed_array.join(" ")
 # end
+
+def reverse_each_word(sentence)
+  sentence.split(" ").collect {|word| word.reverse}.join(" ")
+end
