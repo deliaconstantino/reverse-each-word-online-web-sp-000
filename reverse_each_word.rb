a9528a4f2fb38fb8b9202ff7f48f4b771d3281be
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:34:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,6 +9,7 @@
 #.join emthod with adding " " between each elemetn
 #returns the same sentence with each word reversed in place--trailing punctatin becomes the beginning of it's word
 #return as a string
+require 'pry'
 
 sentence = "Please don't, eat my dear aunt Sally!"
 
