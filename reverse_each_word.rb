a7b3ea34d895d1f153f3b1eb5ed0d747bd5f1e80
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:30:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,6 +10,7 @@
 #returns the same sentence with each word reversed in place--trailing punctatin becomes the beginning of it's word
 #return as a string
 require 'pry'
+sentence = "Please don't, eat my dear aunt Sally!"
 
 def reverse_each_word(sentence)
   org_array = sentence.to_a(" ")
