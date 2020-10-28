7f9bf83bb465b448d439052161cea38c91850d92
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:32:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -19,6 +19,7 @@ def reverse_each_word(sentence)
       backword_word = org_word.reverse
       pushed_array << backword_word
     end
+    binding.pry
   puts pushed_array.join(" ")
   binding.pry
 end
