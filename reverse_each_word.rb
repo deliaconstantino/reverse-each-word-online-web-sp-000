27ac0f34ab573f63df93c18ac5ee6216aa910599
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:22:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
 #takes in a string argument of a sentence
-#convert string to an array, based on " " 
+#reverse_each_word("Hello there, and how are you?")
+  #=> "olleH ,ereht dna woh era ?uoy"
+
+#convert string to an array, based on " "
+#each method
+#use .reverse method on each element of array, and push to new array
+#end each method
+#.join emthod with adding " " between each elemetn
 #returns the same sentence with each word reversed in place--trailing punctatin becomes the beginning of it's word
 #return as a string
 
 reverse_each_word(string)
+  string.to_a(" ")
 
 end
