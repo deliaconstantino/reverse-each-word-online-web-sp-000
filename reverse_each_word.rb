2a8a124e82525d0d0c114eaff32e8525e1da0e3e
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:19:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 #takes in a string argument of a sentence
-
+#convert string to an array, based on " " 
+#returns the same sentence with each word reversed in place--trailing punctatin becomes the beginning of it's word
+#return as a string
 
 reverse_each_word(string)
 
