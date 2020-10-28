506f40b0671f44e4eb708ce4f9e72b1b2e9e8c8f
Delia Constantino <dconstan92@gmail.com>
2020-10-28 14:46:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,6 +9,8 @@
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
-  reversed_array = array.collect do |word|
-    word.reverse
-  end
+    reversed_array = array.collect do |word|
+      word.reverse
+    end
+  reversed_array.join(" ")
+end
